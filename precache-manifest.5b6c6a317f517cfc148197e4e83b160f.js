e7c08811c0b8b9a988a0e86b362c68015b9bb4e3
MaksOvs <maksovsanyuk@gmail.com>
2020-05-10 14:42:46 +0300

    Updates

---
+++ precache-manifest.5b6c6a317f517cfc148197e4e83b160f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddae84c40d115a4574530e2f9a597e21",
+    "url": "/LeeLoo/index.html"
+  },
+  {
+    "revision": "aa7c27b23bce5681d7b8",
+    "url": "/LeeLoo/static/js/2.f0cdc890.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/LeeLoo/static/js/2.f0cdc890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d78ad3d99de97e17da9",
+    "url": "/LeeLoo/static/js/main.524bdb59.chunk.js"
+  },
+  {
+    "revision": "1cd85ad04bcbe7197e6d",
+    "url": "/LeeLoo/static/js/runtime-main.f7db8bcd.js"
+  },
+  {
+    "revision": "ba9f19730fd128c60efcd68adf20369a",
+    "url": "/LeeLoo/static/media/404.ba9f1973.gif"
+  }
+]);
